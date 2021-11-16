be7754b5f69b372a4907116560b0c6f493e843a1
Jdbye <jdbye3@gmail.com>
2021-11-16 14:53:48 +0100

    Update BuildInfo.cs
    Bump version number

--- DoubleTapSpeed/BuildInfo.cs
+++ DoubleTapSpeed/BuildInfo.cs
@@ -4,15 +4,15 @@ namespace DoubleTapRunner
     public static class BuildInfo
     {
 
-        public const string Author = "Psychloor";
+        public const string Author = "Psychloor, Jdbye";
 
         public const string Company = null;
 
-        public const string DownloadLink = "https://github.com/Psychloor/DoubleTapRunner/releases/download/"+Version+"/DoubleTapRunner.dll";
+        public const string DownloadLink = "https://github.com/Jdbye/DoubleTapRunner/releases/download/" + Version+"/DoubleTapRunner.dll";
 
         public const string Name = "Double-Tap Runner";
 
-        public const string Version = "1.4.0";
+        public const string Version = "1.5.0";
 
     }
 
