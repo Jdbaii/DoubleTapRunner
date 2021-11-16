501dd4b535895e2f5dbbc5f0703a081dad55da2d
Jdbye <jdbye3@gmail.com>
2021-11-16 14:45:56 +0100

    InputSetting changed to deobfuscated name

--- DoubleTapSpeed/Utilities.cs
+++ DoubleTapSpeed/Utilities.cs
@@ -14,8 +14,8 @@ namespace DoubleTapRunner
     public static class Utilities
     {
         public static bool GetStreamerMode =>
-            VRCInputManager.Method_Public_Static_Boolean_EnumNPublicSealedvaUnCoHeToTaThShPeVoUnique_0(
-                VRCInputManager.EnumNPublicSealedvaUnCoHeToTaThShPeVoUnique.StreamerModeEnabled);
+            VRCInputManager.Method_Public_Static_Boolean_InputSetting_0(
+                VRCInputManager.InputSetting.StreamerModeEnabled);
 
         public static bool AxisClicked(string axis, string oculusAxis, ref float previous, float threshold)
         {
